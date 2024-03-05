21756133cf4be5920ef147f2a243c1e8709b94e9
zig <zvszig@gmail.com>
2024-03-04 19:17:59 -0800

    activity3 finished product

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,11 +1,13 @@
 const router = require('express').Router()
 const db = require("../models")
+const bcrypt = require('bcrypt')
 
 const { User } = db
 
 router.post('/', async (req, res) => {
     const user = await User.create(req.body)
     res.json(user)
+    passwordDigest: await bcrypt.hash(password, 10)
 })
 
 
